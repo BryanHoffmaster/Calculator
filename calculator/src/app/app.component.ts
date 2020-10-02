0043b6c9a5b7459e7847e5ed3d08a301ad011473
Bryan Hoffmaster <bhoffmaster@blinfo.com>
2020-10-02 11:30:55 -0500

    Implement basic logic flow for operatos and button clicks
    * This will need refactoring in the view, as we can just pass the exact value.

--- calculator/src/app/app.component.ts
+++ calculator/src/app/app.component.ts
@@ -1,16 +1,38 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 
+/** Holds an enumerated list of calculator operations. */
+enum Operator {
+	add,
+	subtract,
+	divide,
+	multiply,
+	clear,
+	equals
+}
+
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+	/** App title. */
 	title = 'calculator';
 
+	/** Public reference to the operators enum. */
+	operator = Operator;
+
+	/** Holds the last value set by the calculator. */
+	private lastValue: number;
+
+	/** Holds the current value in the calculator display. */
+	private currentValue: number;
+
+	/** DOM reference to the calculator display element. */
 	@ViewChild('display', { static: true }) display: ElementRef;
 
-	onButtonClick(element: HTMLButtonElement) {
+	onClickNumber(element: HTMLButtonElement): void {
 		const value = element.innerText;
 		const display = this.display.nativeElement;
 		const atZero = display.innerText === '0';
@@ -21,5 +43,65 @@ export class AppComponent {
 			display.innerText += value;
 		}
 
+		this.lastValue = parseInt(display.innerText);
+	}
+
+	onClickOperator(operator: Operator): void {
+		this.currentValue = parseInt(this.display?.nativeElement?.innerText);
+		switch (operator) {
+			case Operator.add:
+				this.add();
+				break;
+			case Operator.subtract:
+				this.subtract();
+				break;
+			case Operator.multiply:
+				this.multiply();
+				break;
+			case Operator.divide:
+				this.divide();
+				break;
+			case Operator.clear:
+				this.clear();
+				break;
+			case Operator.equals:
+				this.equals();
+				break;
+			default:
+				break;
+		}
+
+		// At the end of every operation, reset the value of the display
+		this.display.nativeElement.innerText = '0';
+	}
+
+	private add(): void {
+		if (!this.lastValue || !this.currentValue) return;
+		console.log('operator clicked');
+	}
+
+	private subtract(): void {
+		if (!this.lastValue || !this.currentValue) return;
+		console.log('operator clicked');
+	}
+
+	private multiply(): void {
+		if (!this.lastValue || !this.currentValue) return;
+		console.log('operator clicked');
+	}
+
+	private divide(): void {
+		if (!this.lastValue || !this.currentValue) return;
+		console.log('operator clicked');
+	}
+
+	private equals(): void {
+		if (!this.lastValue || !this.currentValue) return;
+		console.log('operator clicked');
+	}
+
+	private clear(): void {
+		if (!this.lastValue || !this.currentValue) return;
+		console.log('operator clicked');
 	}
 }
