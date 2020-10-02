f7041d7d25ae16db3afa83e7f09507999d7062d1
Bryan Hoffmaster <bhoffmaster@blinfo.com>
2020-10-02 10:48:09 -0500

    Button clicks now update the display
    * Does not include any logic to prevent adding operator text to the display.

--- calculator/src/app/app.component.ts
+++ calculator/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calculator';
+	title = 'calculator';
+
+	@ViewChild('display', { static: true }) display: ElementRef;
+
+	onButtonClick(element: HTMLButtonElement) {
+		const value = element.innerText;
+		const display = this.display.nativeElement;
+		const atZero = display.innerText === '0';
+
+		if (atZero) {
+			display.innerText = value;
+		} else {
+			display.innerText += value;
+		}
+
+	}
 }
